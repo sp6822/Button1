0bced195a240e9d7da6584308205b56f68e3f106
sp6822 <sp6822@bs.amalne.k12.il>
2025-02-03 23:12:32 +0200

    Button

--- app/src/main/java/com/example/button/MainActivity.java
+++ app/src/main/java/com/example/button/MainActivity.java
@@ -9,26 +9,27 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+import android.widget.Button;
+import android.widget.Button;
 public class MainActivity extends AppCompatActivity {
 
     Button myButton;
     int clickCount = 0;
-    
-        @Override
-        protected void onCreate(Bundle savedInstanceState) {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        myButton = (Button) findViewById(R.id.go);
+        myButton = (Button) findViewById(R.id.button2);
 
     }
-      public void go(view view) {
-            clickCount++;
-            myButton.setText("This is a click number:"+clickCount);
-            if (clickCount==7){
-                myButton.setText("Enough to click. Go to new start!");
-                clickCount=0;
+
+    public void go(View view) {
+        clickCount++;
+        myButton.setText("This is a click number:" + clickCount);
+        if (clickCount == 7) {
+            myButton.setText("Enough to click. Go to new start!");
+            clickCount = 0;
         }
     }
-
-    }
+}
